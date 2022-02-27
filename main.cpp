6567a42300e73c366d49fc8c83a999c2ad589bf3
bin4rym4ge <bin4rym4ge@manjaro>
2022-02-27 07:32:46 -0800

    comments

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
-#include <fstream>								// ofstream
-#include <unistd.h>								// usleep
+#include <fstream>					// ofstream
+#include <unistd.h>					// usleep
 using namespace std;
 
-#define USEC 1000000							// micro second to second multiplier
-#define WAKEOUT "wake"							// default wake output; will be changeable in the future
+#define USEC 1000000					// micro second to second multiplier
+#define WAKEOUT "wake"					// default wake output; will be changeable in the future
 
 int main(int argc, char* argv[]) {
-	int stimer = 5*USEC;						// set default timer to 5 seconds
+	int stimer = 5*USEC;				// set default timer to 5 seconds
     ofstream wakeFiles[argc];
 
     cout << "wake on...\n";
@@ -25,9 +25,9 @@ int main(int argc, char* argv[]) {
 
         for (int i = 1; i < argc; i++)
         {
-            wakeFiles[i].close();				// close wake file
+            wakeFiles[i].close();			// close wake file
         }
 
-        usleep(stimer);							// wait stimer seconds
+        usleep(stimer);					// wait stimer seconds
     }
 }
